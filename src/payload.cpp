90a16e16e7688b8155508f5c97a172ce0516db37
pfedick <patrick@pfp.de>
2019-03-18 17:42:29 +0100

    CentOS6 compat

--- src/payload.cpp
+++ src/payload.cpp
@@ -26,6 +26,9 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#ifndef __FreeBSD__
+#define __FAVOR_BSD
+#endif
 #include <netinet/udp.h>
 #include <arpa/inet.h>
 #include <arpa/nameser.h>
